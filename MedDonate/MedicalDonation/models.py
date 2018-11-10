488b52ab96db87bb16176adcedafca5c64b3c84b
shubham2637 <shubham37@outlook.com>
2018-11-10 23:44:52 +0530

    Models creation done

--- MedDonate/MedicalDonation/models.py
+++ MedDonate/MedicalDonation/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import timedelta
+from datetime import timedelta,datetime
 from .encrption import *
 # Create your models here.
 class Doner(models.Model):
@@ -102,6 +102,27 @@ class medicine(models.Model):
         return (f"{self.name} {self.tradename}")
 
 
-class medicine_legger(models.Model):
-    #donor_id = models.ForeignKey(Doner,)
-    pass
+class medicine_ledger(models.Model):
+    donor_id = models.ForeignKey(Doner,blank=True,on_delete=models.CASCADE)
+    collector_id = models.ForeignKey(Collector,blank=True, on_delete=models.CASCADE)
+    medicine_id = models.ForeignKey(medicine,blank=True, on_delete=models.CASCADE)
+    acceptor_id = models.ForeignKey(Acceptor,blank=True, on_delete=models.CASCADE)
+    indate = models.DateTimeField(default=datetime.now(tz=None))
+    outdate = models.DateTimeField(default=datetime.now(tz=None))
+    medicine_count = models.PositiveIntegerField(default=1)
+
+
+    def __str__(self):
+        return (f"{self.medicine_id} {self.medicine_count} {self.indate} {self.outdate}")
+
+
+class job(models.Model):
+    donor_id = models.ForeignKey(Doner,blank=True,on_delete=models.CASCADE)
+    collector_id = models.ForeignKey(Collector,blank=True, on_delete=models.CASCADE)
+    acceptor_id = models.ForeignKey(Acceptor,blank=True, on_delete=models.CASCADE)
+    medicine_id = models.ForeignKey(medicine,blank=True, on_delete=models.CASCADE)
+    vehicle_id = models.ForeignKey(vehicle,blank=True,on_delete=models.CASCADE)
+    date = models.DateTimeField(default=datetime.now(tz=None))
+
+    def __str__(self):
+        return(f"{self.date} {self.donor_id} {self.collector_id} {self.acceptor_id} {self.medicine_id} {self.vehicle_id}")
